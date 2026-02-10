b4373764978c376ff601415d7ab8219237a8b01c
AthiFiz <athif.rasheedh@gmail.com>
2026-02-10 22:44:41 +0530

    enabled idempotent producer

--- product/src/main/java/com/fiz/product/config/KafkaConfig.java
+++ product/src/main/java/com/fiz/product/config/KafkaConfig.java
@@ -38,6 +38,12 @@ public class KafkaConfig {
     @Value("${spring.kafka.producer.properties.request.timeout.ms}")
     private String requestTimeout;
 
+    @Value("${spring.kafka.producer.properties.enable.idempotence}")
+    private Boolean enableIdempotence;
+
+    @Value("${spring.kafka.producer.properties.max.in.flight.request.per.connection}")
+    private Integer inflightRequest;
+
     Map<String, Object> producerConfig(){
         Map<String, Object> config = new HashMap<>();
 
@@ -48,6 +54,8 @@ public class KafkaConfig {
         config.put(ProducerConfig.DELIVERY_TIMEOUT_MS_CONFIG, deliveryTimeout);
         config.put(ProducerConfig.LINGER_MS_CONFIG, linger);
         config.put(ProducerConfig.REQUEST_TIMEOUT_MS_CONFIG, requestTimeout);
+        config.put(ProducerConfig.ENABLE_IDEMPOTENCE_CONFIG, enableIdempotence);
+        config.put(ProducerConfig.MAX_IN_FLIGHT_REQUESTS_PER_CONNECTION, inflightRequest);
 
         return config;
     }
